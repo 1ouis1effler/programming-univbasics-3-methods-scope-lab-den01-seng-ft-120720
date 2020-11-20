555ea382596ceae1f6b66fdafa2d04f7e9935cf6
Louis Leffler <louisleffler@gmail.com>
2020-11-20 18:52:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
-  it "puts out 'It's-a me, Mario!'" do
+  it "puts out 'It's-a me, Mario!'"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
@@ -9,13 +9,13 @@ end
 
 def toadstool
   puts status
- "toadstool" do
+ "toadstool"
   it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
     expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
   end
 end
 
-  "all_phrases" do
+  "all_phrases"
   it "puts out all of the previous catch phrases" do
     expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
     expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout

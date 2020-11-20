652f92677734a8a2b7879bab35ed8452a31de86e
Louis Leffler <louisleffler@gmail.com>
2020-11-20 18:36:20 +0000

    Done.

---
+++ phrase.rb
@@ -0,0 +1,14 @@
+
+def toadstool
+ describe "toadstool" do
+  it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
+    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
+  end
+end
+
+describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
+end
+##
